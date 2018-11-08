d45c7c8baee86c50ca40155213378971649bf1ed
hdakwd <egotism49@gmail.com>
2018-11-09 00:38:07 +0900

    DataBase test.

---
+++ app/src/main/java/pkg/json/db_test/MainActivity.java
@@ -0,0 +1,23 @@
+package pkg.json.db_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSaveButtonClick(View view) {
+        //未入力！！！
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //未入力！！！
+    }
+}
